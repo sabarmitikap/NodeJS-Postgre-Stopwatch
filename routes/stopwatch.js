dbbd43ea5ff363f5a038acb7e4c2da946f7afd24
sabarmitikap <sabar.itikaf62@gmail.com>
2021-10-12 14:52:38 +0700

    Update CRUD

--- routes/stopwatch.js
+++ routes/stopwatch.js
@@ -6,5 +6,7 @@ const router = express.Router();
 router.post('/createStopwatch', stopwatchController.createStopwatch)
 router.get('/getStopwatch/:id', stopwatchController.getStopwatch)
 router.get('/getStopwatchTimestamp/:id', stopwatchController.getStopwatchTimestamp)
+router.get('/getStopwatch/:id', stopwatchController.getStopwatchName)
+router.get('/getStopwatchTimestamp/:id', stopwatchController.getStopwatchStatus)
 
 module.exports = router
